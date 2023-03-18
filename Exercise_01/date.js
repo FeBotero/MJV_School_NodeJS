18e8f7b2dca9cdcc47db429a560a16d077950c38
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-18 17:02:29 -0400

    Initial commit

---
+++ Exercise_01/date.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const today = Date.now();
+
+console.log(`Pela constante date hoje é ${date}`);
+console.log(
+  `Pela constante today que representa em milisegundos a partir de 01/01/1970, hoje é ${today}`
+);
---
+++ Exercise_01/helloWorld.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+const n1 = 2;
+const n2 = 2;
+
+console.log(`O resultado da soma de ${n1} + ${n2} é :${n1 + n2}`);
