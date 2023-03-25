0fd5f32b4cba7ab55fe7d2c500921e7c1795a8bf
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-25 19:48:33 -0400

    Exercise_03_04

--- Exercise_03a/src/routes/index.ts
+++ Exercise_03a/src/routes/index.ts
@@ -22,10 +22,22 @@ router.put("/:id", (req: Request, res: Response) => {
     (product) => product.id === JSON.parse(req.params.id)
   );
   if (product === -1) {
-    res.status(400).send({ message: "Dados inválidos para cadastro!" });
+    res.status(400).send({ message: "Dados inválidos para atualizar!" });
   } else {
     products[product] = body;
     res.status(200).send({ message: "Produto atualizado com sucesso!" });
+  }
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+  const product = products.findIndex(
+    (product) => product.id === JSON.parse(req.params.id)
+  );
+  if (product === -1) {
+    res.status(400).send({ message: "Dados inválidos para exclusão!" });
+  } else {
+    products.splice(product, 1);
+    res.status(200).send({ message: "Produto excluído com sucesso!" });
   }
 });
 
